3b78d4761db9ad8ecd66a1b22d53bcbcf94cbd83
ADITYA <120659983+ADITYA462@users.noreply.github.com>
2023-06-16 15:02:44 +0530

    Add files via upload

---
+++ Searching & Sorting.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Implementation of Binary Search
+
+# In[1]:
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+
+# # Implementation of Merge Sort
+
+# In[2]:
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+
+# # Implementation of Quick Sort
+
+# In[4]:
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+# # Implementation of Insertion Sort
+
+# In[5]:
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+
+# # Sorting a list of strings:
+
+# In[6]:
+
+
+def sort_strings(string_list):
+    return sorted(string_list, key=lambda x: tuple(x.split()))
+
+strings = ["apple", "banana", "cat", "dog", "apple pie", "banana bread"]
+sorted_strings = sort_strings(strings)
+print(sorted_strings)
+
+
+# In[ ]:
+
+
+
+
